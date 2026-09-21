8f66d7cd87cc56b22662cca4900b5a64f7edcd7d
macbookpro <nguyenlevietanh83@gmail.com>
2026-09-21 23:30:26 +0700

    Nộp bài tập buổi 6

---
+++ BT_CSLT/B6_Bai03.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Buoi_3
+{
+	public class B6_Bai03
+	{
+		public B6_Bai03()
+		{
+		}
+	}
+}
+
---
+++ BT_CSLT/buoi6.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Buoi_3
+{
+	public class buoi6
+	{
+		public buoi6()
+		{
+		}
+	}
+}
+
